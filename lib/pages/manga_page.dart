9daacc5f68ceff7c6101d5247b1d8cdef0cb608a
Necozz <96022728+Necozz@users.noreply.github.com>
2024-10-08 12:55:43 +0200

    Update asset paths and add manga page description

--- lib/pages/manga_page.dart
+++ lib/pages/manga_page.dart
@@ -13,18 +13,19 @@ class MangaPage extends StatelessWidget {
           child: Text('Manga Page'),
         ),
         backgroundColor: const Color(0xFFF5BFFF).withOpacity(0.1),
-                systemOverlayStyle: SystemUiOverlayStyle.light,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
       body: Container(
         color: Theme.of(context).scaffoldBackgroundColor,
         child: const Padding(
           padding: EdgeInsets.all(16.0),
           child: Align(
-        alignment: Alignment.topLeft,
-        child: Text(
-          " ",
-          textAlign: TextAlign.left,
-        ),
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Manga are comics or graphic novels originating from Japan. Most manga conform to a style developed in Japan in the late 19th century, and the form has a long prehistory in earlier Japanese art.",
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 16.0),
+            ),
           ),
         ),
       ),
