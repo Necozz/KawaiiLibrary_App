68bcc8c582b71a8b7974ce573f5101d69f605961
Necozz <96022728+Necozz@users.noreply.github.com>
2024-10-08 12:19:00 +0200

    yeet

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent, brightness: Brightness.dark),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurpleAccent, brightness: Brightness.dark),
         useMaterial3: true,
       ),
       home: const MainPage(),
